70e65b6a6dc18254811ec01976318d64b8cc99ac
ashaut <34644578+ashaaut@users.noreply.github.com>
2022-03-20 17:40:20 +0530

    created new component for tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,76 @@
 import './../assets/css/app.css';
-
+import Todo from './Todo'
+import Tasks from './Tasks'
 function App() {
   return (
     <div className="App">
-      Hello World
+      <h1>TodoMatic</h1>
+      <div className='todo'>
+        <Todo/>
+      </div>
+      <div className='state-tasks'>
+        <button type='button'>All tasks</button>
+        <button type='button'>Active tasks</button>
+        <button type='button'>Completed tasks</button>
+      </div>
+      <div className='tasks'>
+        <Tasks/>
+      </div>
+      {/* <div className='list-tasks'>
+        <ul
+          role="list"
+        >
+          <li >
+            <div >
+              <input type="checkbox" defaultChecked={true} />
+              <label >
+                Eat
+              </label>
+            </div>
+            <div >
+              <button type="button">
+                Edit
+              </button>
+              <button type="button" >
+                Delete
+              </button>
+            </div>
+          </li>
+          <li >
+            <div >
+              <input type="checkbox" />
+              <label>
+                Sleep
+              </label>
+            </div>
+            <div >
+              <button type="button" >
+                Edit
+              </button>
+              <button type="button" className="btn btn__danger">
+                Delete
+              </button>
+            </div>
+          </li>
+          <li >
+            <div >
+              <input id="todo-2" type="checkbox" />
+              <label >
+                Repeat
+              </label>
+            </div>
+            <div>
+              <button type="button" >
+                Edit
+              </button>
+              <button type="button" >
+                Delete
+              </button>
+            </div>
+          </li>
+        </ul> */}
+
+      {/* </div> */}
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+export  default function Todo() {
+    return (
+        <div>
+            <form>
+                <h2>What needs to be done?</h2>
+                <div>
+                    <input type="text" />
+                    <button type='submit'>Add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
